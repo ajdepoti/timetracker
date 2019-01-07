7343cb40aa655aebd3b5ec7fc4c3c4116145fa05
D4 <36803803+ajdepoti@users.noreply.github.com>
2019-01-06 21:32:31 -0500

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HoursComponent } from './hours/hours.component';
+import { TagsComponent } from './tags/tags.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'hours', component: HoursComponent },
+  { path: 'tags', component: TagsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HoursComponent, TagsComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HoursComponent } from './hours/hours.component';
-import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HoursComponent,
-    TagsComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
